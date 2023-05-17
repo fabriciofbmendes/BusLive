1c53b011968a53079f58a0dedeecfbbbe78ea8eb
Fabricio Terra <lima.de.terra@hotmail.com>
2023-05-16 22:39:04 -0300

    Revert "Revert "Revert "Finalizando as funcionalidades de mapa do sistema"""
    This reverts commit cdca95a04c6d6c16d0c62637d26264f6421cdb59.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState, useRef,useCallback } from "react";
-import { Pressable,Alert, Linking, Text, View, StatusBar,Modal } from "react-native";
-import Dropdown, { DropdownOptionsProps } from "./componentes"
+import React, { useEffect, useState, useRef } from "react";
+import { Pressable,Alert, Linking, Text, View } from "react-native";
+import Dropdown from "./componentes"
 import reload from "./Main";
 import { useTimeout } from './useTimeout';
 import VehicleLocationService from './VehicleLocationService';
 import Statusbar from "./componentes/statusbar";
-import axios from 'axios';
+
 import MapView, {
   Callout,
   Marker,
@@ -18,10 +18,7 @@ import { RectButton, TextInput } from "react-native-gesture-handler";
 import * as Location from "expo-location";
 import styles from "../src/styles";
 import stylinho from "../src/SearchBar"
-//import { StatusBar } from "expo-status-bar";
-import {getVehicleLocation}  from "../src/TCC/BusApi";
-import { LocationObject } from "expo-location";
-
+import { StatusBar } from "expo-status-bar";
 
 function guidGenerator(){
   var S4 = function(){
@@ -30,6 +27,11 @@ function guidGenerator(){
   return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 }
 
+const submit = () => {
+  setTimeout(() => {
+    console.log("teste2")
+}, 5000)
+}
 interface Bus{
   IsActive : boolean;
   latitude: number;
@@ -44,30 +46,12 @@ let initialRegion = {
   longitudeDelta: 0.05,
 };
 
-let initialPoint = {
-  coords:{
-    latitude: -21.425300,
-    longitude: -45.949712,
-  }
-}
-
-
 export default function App() {
   //#region constantes
 const mapViewRef = useRef<MapView>(null);
 const [Busmarkers, setBusMarkers] = useState<LatLng[]>([]);
 const [UserMarkers, setUserMarkers] = useState<LatLng[]>([]);
 const [region, setRegion] = useState<Region>();
-const [location, setLocation] = useState<LocationObject | null>(null);
-const [vehicleLocation, setVehicleLocation] = useState<any| null>(null);
-const [selectedOption, setSelectedOption] = useState<string>();
-const [showAlert, setShowAlert] = useState(false);
-
-
-
-const clearMarkers = () => {
-  setBusMarkers([]);
-};
 
 //#endregion
   const getCurrentPosition = async () => {
@@ -75,93 +59,66 @@ const clearMarkers = () => {
     if (status !== "granted") {
       Alert.alert("Ops!", "Permissão de acesso a localização negada.");
     }
+    const clearMarkers = () => {
+      setUserMarkers([]);
+      setBusMarkers([]);
+    };
 
     if(status == "granted"){  
       let {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
 
-      setRegion({ latitude, longitude, latitudeDelta: 0.03, longitudeDelta: 0.03 });
+      setRegion({ latitude, longitude, latitudeDelta: 0.005, longitudeDelta: 0.005 });
+      clearMarkers();
+
+      let marker = {
+         latitude
+         ,longitude
+       }
+       
+      //setUserMarkers([marker]);
+      let bool = 1;
+
+      if(bool == 1 ){
+        
+        latitude = -21.427000
+        longitude = -45.947000
+  
+        let busmarker ={
+          latitude,
+          longitude
+        } 
+        
+        setRegion({ latitude, longitude, latitudeDelta: 0.005, longitudeDelta: 0.005 });
+  
+        setBusMarkers([busmarker]);
+      }
     } 
   };
 
-  getCurrentPosition();
-
-  const handleValueChange = useCallback<DropdownOptionsProps["onValueChange"]>(
-    async (value :string) => {
-      setSelectedOption(value);
-      await setShowAlert(true);
-    },
-    [selectedOption]
-  );
-
-    const attBus = useCallback(
-      () => {
-        clearMarkers();
-        let {
-          coords: { latitude, longitude }
-        } = initialPoint;
-
-        if (!(vehicleLocation === null)) {
-          latitude = parseFloat(vehicleLocation.latitude);
-          longitude = parseFloat(vehicleLocation.longitude);
-        }
-
-        let busmarker = {
-          latitude,
-          longitude
-        };
-        setBusMarkers([busmarker]);
-      },
-      [vehicleLocation]  
-  );
-
-
-    useEffect(() => {
-      let timeoutId: NodeJS.Timeout;
-    
-      if (showAlert) {
-        setShowAlert(true);
-    
-        timeoutId = setTimeout(() => {
-          setShowAlert(false); 
-        }, 6000);
-      }
-      attBus();
-      return () => {
-        if (timeoutId) {
-          clearTimeout(timeoutId);
-        }
-      };
-    }, [showAlert]);
-
-
-
-   useEffect(() => {
-     const intervalId = setInterval(async () => {
-       const newLocation = await Location.getCurrentPositionAsync({});
-      setLocation(newLocation);
-    }, 3000);
-
-     return () => clearInterval(intervalId);
-   }, []);
-
-   useEffect(() => {
-      const intervalId = setInterval(async () => {
-      const vehicleId = selectedOption; 
-      const newLocation = JSON.stringify(await getVehicleLocation(vehicleId));
-      if(!(newLocation===undefined || newLocation==JSON.stringify(vehicleLocation))){
-        await setVehicleLocation(JSON.parse(newLocation));
-        await attBus();
-      }
-    }, 3000);
-    return () => clearInterval(intervalId);
-  }, [selectedOption,vehicleLocation]);
+  // const MapScreen = ({ vehicleId}) => {
+  //   const [vehicleLocation, setVehicleLocation] = useState(null);
+  
+  //   useEffect(() => {
+  //     const fetchVehicleLocation = async () => {
+  //       const location = await VehicleLocationService.getVehicleLocation(vehicleId);
+  //       setVehicleLocation(location);
+  //     };
+  
+  //     fetchVehicleLocation();
+  //     // Você pode ajustar o intervalo de atualização de acordo com suas necessidades
+  //     const intervalId = setInterval(fetchVehicleLocation, 5000);
+  
+  //     return () => {
+  //       clearInterval(intervalId);
+  //     };
+  //   }, [vehicleId]);
 
   return (
     <View style={stylinho.content}>
       <Statusbar/>
-      <Dropdown selectedOption={selectedOption} onValueChange={handleValueChange}/>
+      <Dropdown/>
       <MapView
         ref={mapViewRef}
         provider={PROVIDER_GOOGLE}
@@ -172,13 +129,24 @@ const clearMarkers = () => {
         loadingEnabled={true}
         showsUserLocation={true}
         onUserLocationChange={()=>{}}
+        onMapLoaded={submit}
       >
-           
+        
+      {UserMarkers.map((marker) => (
+          <Marker
+          pinColor="Red"
+            key={guidGenerator()}
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}
+          />
+        ))}
+        
     {Busmarkers.map((marker) => (
           <Marker
           tracksViewChanges={true}
-            //image={require("./imagens/MarkerOnibusFinal.png")}
-            pinColor="Red"
+            image={require("./imagens/MarkerOnibusFinal.png")}
             key={guidGenerator()}
             coordinate={{
               latitude: marker.latitude,
@@ -188,10 +156,6 @@ const clearMarkers = () => {
         ))}
       </MapView>
       {/* <StatusBar hidden={true}/> */}
-      <Modal visible={showAlert} onRequestClose={() => {}}>
-         <Text>Estamos buscando a informação do seu ônibus</Text>
-       </Modal>
-
       
     </View>
   )}
